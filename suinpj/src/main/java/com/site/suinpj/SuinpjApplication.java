628fa925208290c8db16196d4531806c08905fdd
inni0122 <suve1y34@gmail.com>
2024-03-13 12:58:17 +0900

    first commit

---
+++ suinpj/src/main/java/com/site/suinpj/SuinpjApplication.java
@@ -0,0 +1,13 @@
+package com.site.suinpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuinpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuinpjApplication.class, args);
+	}
+
+}
---
+++ suinpj/src/test/java/com/site/suinpj/SuinpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.suinpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuinpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
