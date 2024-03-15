21ebc3289e8c2eb64a099866627d0579cee36a7e
inni0122 <suve1y34@gmail.com>
2024-03-16 00:50:49 +0900

    채팅방/메시지를 위한 entity, dao, service 클래스 생성 및 수정

---
+++ suinpj/src/main/java/com/site/suinpj/entity/ChatRoom.java
@@ -0,0 +1,12 @@
+package com.site.suinpj.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class ChatRoom {
+
+}
--- suinpj/src/main/java/com/site/suinpj/service/ChatService.java
+++ suinpj/src/main/java/com/site/suinpj/service/ChatService.java
@@ -1,5 +1,11 @@
 package com.site.suinpj.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ChatService {
 
 }
