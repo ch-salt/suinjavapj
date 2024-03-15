7d0348071b319a853cc257b53eb17998066bcf5f
inni0122 <suve1y34@gmail.com>
2024-03-16 00:30:49 +0900

    사용될 패키지 및 Controller, Service 부분 클래스 생성

---
+++ suinpj/src/main/java/com/site/suinpj/controller/ChatController.java
@@ -0,0 +1,12 @@
+package com.site.suinpj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.site.suinpj.service.ChatService;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+}
---
+++ suinpj/src/main/java/com/site/suinpj/service/ChatService.java
@@ -0,0 +1,5 @@
+package com.site.suinpj.service;
+
+public class ChatService {
+
+}
